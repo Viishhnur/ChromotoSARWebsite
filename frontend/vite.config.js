c39272b1974192676f8dbc9ee424ac53a1830312
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2024-12-27 00:13:17 +0530

    Updated vit.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// this file is used to configure the frontend
 export default defineConfig({
   plugins: [react()],
   server : {
