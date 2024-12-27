3eb2c8f79376ff750017d91d9f49283733ed745b
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2024-12-27 13:25:15 +0530

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // this file is used to configure the frontend
 export default defineConfig({
+  base: './' , // Use relative paths for assets
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000
-  },
-  base: './'  // Use relative paths for assets
+  }
 })
 
